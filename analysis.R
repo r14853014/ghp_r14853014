dbddd76a053688125eb7c0d089695f3cf4ff49e0
Loren Nicole Foster Torres <nicolefoster@Lorens-MacBook-Air.local>
2026-05-28 20:41:06 +0800

    assignment

---
+++ analysis.R
@@ -0,0 +1,13 @@
+# for assignment 3
+library(dplyr)
+setwd("~/Documents/ghp_r14853014")
+dengue_data <- read.csv("dengue_assignment.csv")
+
+# Calculate total cases reported in 2023
+total_cases_2023 <- sum(
+  dengue_data$case_number[dengue_data$year == 2023],
+  na.rm = TRUE
+)
+
+# Print the result
+print(total_cases_2023)
