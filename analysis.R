1df96231149d51e20fba0d9948110b30779f7ef6
Loren Nicole Foster Torres <nicolefoster@Lorens-MacBook-Air.local>
2026-05-28 21:06:25 +0800

    second commit

--- analysis.R
+++ analysis.R
@@ -1,7 +1,10 @@
+# Nicole Foster, 2026
 # for assignment 3
+
 library(dplyr)
 setwd("~/Documents/ghp_r14853014")
 dengue_data <- read.csv("dengue_assignment.csv")
+
 
 # Calculate total cases reported in 2023
 total_cases_2023 <- sum(
